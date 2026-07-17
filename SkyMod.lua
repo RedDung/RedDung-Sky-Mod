26f0d4fadb017fdfc194b6caa917a4bb69a49768
Dũng <dungred411@gmail.com>
2026-07-17 14:49:23 +0700

    Add sky modification script to change skybox
    This script modifies the sky in the game by setting a custom skybox using a specified asset ID.

--- SkyMod.lua
+++ SkyMod.lua
@@ -1,1 +1,15 @@
 
+-- Script thay đổi bầu trời - RedDung0411
+local Lighting = game:GetService("Lighting")
+local sky = Lighting:FindFirstChild("Sky") or Instance.new("Sky", Lighting)
+
+local assetId = "rbxassetid://81882138179294" -- ID ảnh của bạn
+
+sky.SkyboxBk = assetId
+sky.SkyboxDn = assetId
+sky.SkyboxFt = assetId
+sky.SkyboxLf = assetId
+sky.SkyboxRt = assetId
+sky.SkyboxUp = assetId
+
+print("Đã load Sky Mod thành công!")
